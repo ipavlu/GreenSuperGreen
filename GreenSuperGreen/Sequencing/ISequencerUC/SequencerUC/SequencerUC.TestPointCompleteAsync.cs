c7caaeeabcd1515f8228b78a00fda17e60ece500
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-02-15 02:05:06 +0100

    awaiting testing point in unit test and setting completion or failure

---
+++ GreenSuperGreen/Sequencing/ISequencerUC/SequencerUC/SequencerUC.TestPointCompleteAsync.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+using System.Threading.Tasks;
+
+namespace GreenSuperGreen.Sequencing
+{
+	public static partial class SequencerUC
+	{
+		public
+		static
+		async
+		Task<IProductionPointUC>
+		TestPointCompleteAsync<TEnum>(this	ISequencerUC sequencer,
+											TEnum registration,
+											object testArg = null,
+											SeqContinuationUC context = SeqContinuationUC.OnCapturedContext)
+		where TEnum : struct
+		{
+			IProductionPointUC prodPoint = await sequencer.TestPointAsync(registration);
+			prodPoint?.Complete(testArg, context);
+			return prodPoint;
+		}
+	}
+}
