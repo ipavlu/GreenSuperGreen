b33fbd46c50fa8e67acb879d46c9d20d61db8743
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-03-02 13:36:09 +0100

    General Task Constatns

---
+++ GreenSuperGreen/GeneralExtensions/Task/TaskConstants.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+// ReSharper disable CheckNamespace
+
+namespace GreenSuperGreen
+{
+	public static class TaskConstants
+	{
+		public static Task<bool> TaskTrue { get; } = Task.FromResult(true);
+		public static Task<bool> TaskFalse { get; } = Task.FromResult(false);
+		public static Task<bool?> TaskNullableTrue { get; } = Task.FromResult<bool?>(true);
+		public static Task<bool?> TaskNullableFalse { get; } = Task.FromResult<bool?>(false);
+
+		private static Task GetTaskCanceled()
+		{
+			var tcs = new TaskCompletionSource<object>();
+			tcs.SetCanceled();
+			return tcs.Task;
+		}
+
+		public static Task TaskCanceled { get; } = GetTaskCanceled();
+
+		public static Task<int?> NullInt { get; } = Task.FromResult((int?)null);
+	}
+}
