9b2ebc019e9aca42f084e6257c673f35f5eddee2
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-03-02 14:31:22 +0100

    TimerProcessorUC: timing classes internal for UnifiedConcurrency classes

---
+++ GreenSuperGreen/UnifiedConcurrency/TimerProcessorUC/TimerProcessorUC.cs
@@ -0,0 +1,15 @@
+﻿using GreenSuperGreen.Timing;
+
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+
+namespace GreenSuperGreen.UnifiedConcurrency
+{
+	internal static class TimerProcessorUC
+	{
+		public static int Period { get; } = 10;
+		public static IRealTimeSource RealTimeSource { get; } = new RealTimeSource();
+		public static ITickGenerator TickGenerator { get; } = new TickGenerator(Period); 
+		public static ITimerProcessor TimerProcessor { get; } = new TimerProcessor(RealTimeSource, TickGenerator);
+	}
+}
