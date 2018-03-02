e763f23d561c4daa17753359631ef4580a581192
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-03-02 13:14:24 +0100

    IRealTimeSource/RealTimeSource - allows to rerouse real time source, if there are more, but necessary in unit tests to mimick passing real time

---
+++ GreenSuperGreen/Timing/RealTimeSource/RealTimeSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// ReSharper disable ForCanBeConvertedToForeach
+// ReSharper disable ExpressionIsAlwaysNull
+// ReSharper disable ArgumentsStyleLiteral
+// ReSharper disable RedundantExtendsListEntry
+// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace GreenSuperGreen.Timing
+{
+	public interface IRealTimeSource
+	{
+		DateTime GetUtcNow();
+	}
+
+	public class RealTimeSource : IRealTimeSource
+	{
+		public DateTime GetUtcNow() => DateTime.UtcNow;
+	}
+}
