63f3cb6dbd29b7c774b9a9dc26a380a17497f8b3
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-03-02 13:12:12 +0100

    public static async Task<Task> WrapIntoTask(this Task t) public static async Task<Task<TResult>> WrapIntoTask<TResult>(this Task<TResult> t) - allows to await task and also examine the task after await, which would not be accessible if not stored - primary use in unit tests, code is simpler, but takes memory and adds await chain state machine steps
    //task is not accessible, must be stored if later examination is required
    await SomethingAsync();
    
    //but now the code gets ugly and verbose
    Task t = SomethingAsync();
    await t;
    Assert.IsTrue(t.IsComplete);
    
    //how about this
    Task t = await SomethingAsync().WrapIntoTask();
    Assert.IsTrue(t.IsComplete);
    
    //or even this
    Assert.IsTrue((await SomethingAsync().WrapIntoTask()).IsComplete);

---
+++ GreenSuperGreen/GeneralExtensions/Task/TaskExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// ReSharper disable CheckNamespace
+
+namespace GreenSuperGreen
+{
+	public static class TaskExtensions
+	{
+		/// <summary>
+		/// Primary use in unit tests, code is simpler,
+		/// but costs memory allocations and adds extra steps into await chain state machine
+		/// </summary>
+		public static async Task<Task> WrapIntoTask(this Task t)
+		{
+			if (t == null) throw new ArgumentNullException(nameof(t));
+			await t;
+			return t;
+		}
+
+		/// <summary>
+		/// Primary use in unit tests, code is simpler,
+		/// but costs memory allocations and adds extra steps into await chain state machine
+		/// </summary>
+		public static async Task<Task<TResult>> WrapIntoTask<TResult>(this Task<TResult> t)
+		{
+			if (t == null) throw new ArgumentNullException(nameof(t));
+			await t;
+			return t;
+		}
+	}
+}
