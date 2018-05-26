d249ec9c01d1f1f1c0a07985d279f1d92a9ea89e
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-05-26 23:01:28 +0200

    Disable obsolet warnings for ISimpleLockUC until it will be removed

--- GreenSuperGreen/UnifiedConcurrency/ILockUC/ILockUC.cs
+++ GreenSuperGreen/UnifiedConcurrency/ILockUC/ILockUC.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+// ReSharper disable UnusedMember.Global
 // ReSharper disable RedundantExtendsListEntry
 // ReSharper disable CheckNamespace
 // ReSharper disable InconsistentNaming
@@ -7,28 +8,30 @@
 
 namespace GreenSuperGreen.UnifiedConcurrency
 {
-	public interface ILockUC : ILockUC<EntryBlockUC, object>, ISimpleLockUC<EntryBlockUC, object>, ISimpleLockUC
-	{
-	}
+	#pragma warning disable 618 //disable obsolete warning
+		public interface ILockUC : ILockUC<EntryBlockUC, object>, ISimpleLockUC<EntryBlockUC, object>, ISimpleLockUC
+		{
+		}
+	#pragma warning restore 618 //enable obsolete warning
 
 
-	[Obsolete("Scheduled for removal in version 2.0.0.0")]
+	[Obsolete("Scheduled for removal in version 2.0.0")]
 	public interface ISimpleLockUC : ISimpleLockUC<EntryBlockUC, object>
 	{
 	}
 
-	public interface ILockUC<out TEntryBlock, TArg>
-	:	ISimpleLockUC<TEntryBlock, TArg>
-		where TEntryBlock : IEntryBlockUC<TArg>
-	{
-		new SyncPrimitiveCapabilityUC Capability { get; }
+	#pragma warning disable 618 //disable obsolete warning
+		public interface ILockUC<out TEntryBlock, TArg> : ISimpleLockUC<TEntryBlock, TArg> where TEntryBlock : IEntryBlockUC<TArg>
+		{
+			new SyncPrimitiveCapabilityUC Capability { get; }
 
-		new TEntryBlock Enter();
-		new TEntryBlock TryEnter();
-		new TEntryBlock TryEnter(int milliseconds);
-	}
+			new TEntryBlock Enter();
+			new TEntryBlock TryEnter();
+			new TEntryBlock TryEnter(int milliseconds);
+		}
+	#pragma warning restore 618 //enable obsolete warning
 
-	[Obsolete("Scheduled for removal in version 2.0.0.0")]
+	[Obsolete("Scheduled for removal in version 2.0.0")]
 	public interface ISimpleLockUC<out TEntryBlock, TArg> where TEntryBlock : IEntryBlockUC<TArg>
 	{
 		SyncPrimitiveCapabilityUC Capability { get; }
