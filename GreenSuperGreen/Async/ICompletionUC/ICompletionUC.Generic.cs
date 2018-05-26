fbff4f48535f472e77aac019cfac895a11fc2b6f
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-05-26 23:02:29 +0200

    comments update to ICompletionUC interface

--- GreenSuperGreen/Async/ICompletionUC/ICompletionUC.Generic.cs
+++ GreenSuperGreen/Async/ICompletionUC/ICompletionUC.Generic.cs
@@ -1,17 +1,13 @@
 ﻿using System.Runtime.CompilerServices;
 using GreenSuperGreen.IdentifierGenerators;
 
-// ReSharper disable InconsistentNaming
 // ReSharper disable RedundantExtendsListEntry
 // ReSharper disable CheckNamespace
-// ReSharper disable UnusedMemberInSuper.Global
 
 namespace GreenSuperGreen.Async
 {
 	/// <summary>
-	/// Please note that <see cref="ICompletionUC{TResult}"/> is complete .net awaitable iterface with result,
-	/// it has everything the await keyword returin result needs. Making own awaitable class with result
-	/// is greatly simplified with <see cref="ACompletionUC{TImplementer}"/>
+	/// Please note that <see cref="ICompletionUC{TResult}"/> is complete .net awaitable iterface with result,  it has everything the await keyword returin result needs.
 	/// </summary>
 	public interface ICompletionUC<out TResult> : ISimpleCompletionUC, INotifyCompletion, ICriticalNotifyCompletion, IUniqueID
 	{
--- GreenSuperGreen/Async/ICompletionUC/ICompletionUC.cs
+++ GreenSuperGreen/Async/ICompletionUC/ICompletionUC.cs
@@ -1,18 +1,13 @@
 ﻿using System.Runtime.CompilerServices;
 using GreenSuperGreen.IdentifierGenerators;
 
-// ReSharper disable UnusedMethodReturnValue.Global
-// ReSharper disable InconsistentNaming
 // ReSharper disable RedundantExtendsListEntry
 // ReSharper disable CheckNamespace
-
 
 namespace GreenSuperGreen.Async
 {
 	/// <summary>
-	/// Please note that <see cref="ICompletionUC"/> is complete .net awaitable iterface,
-	/// it has everything the await keyword needs. Making own awaitable class
-	/// is greatly simplified with <see cref="ACompletionUC{TImplementer}"/>.
+	/// Please note that <see cref="ICompletionUC"/> is complete .net awaitable iterface,  it has everything the await keyword needs.
 	/// </summary>
 	public interface ICompletionUC : ISimpleCompletionUC, INotifyCompletion, ICriticalNotifyCompletion, IUniqueID
 	{
