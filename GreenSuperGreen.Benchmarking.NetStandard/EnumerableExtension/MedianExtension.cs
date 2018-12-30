6965a57eee223fecccc12a7c83449e787b151249
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-12-30 05:54:42 +0100

    Median

---
+++ GreenSuperGreen.Benchmarking.NetStandard/EnumerableExtension/MedianExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GreenSuperGreen.Benchmarking
+{
+	public static class MedianExtension
+	{
+		public static double Median(this IEnumerable<double> ie)
+		{
+			double[] sortedNumbers = ie.OrderBy(n => n).ToArray();
+			int halfIndex = sortedNumbers.Length / 2;
+			double median = (sortedNumbers.Length % 2) == 0
+					? (sortedNumbers[halfIndex] + sortedNumbers[halfIndex - 1]) / 2.0
+					: sortedNumbers[halfIndex]
+				;
+			return median;
+		}
+	}
+}
