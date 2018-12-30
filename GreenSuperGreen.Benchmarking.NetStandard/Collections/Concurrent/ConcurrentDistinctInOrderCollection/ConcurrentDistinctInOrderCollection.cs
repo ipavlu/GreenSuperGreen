8f859954415e36833da1588a4a89e9404af9b99a
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-12-30 05:54:19 +0100

    ConcurrentDistinctInOrderCollection

---
+++ GreenSuperGreen.Benchmarking.NetStandard/Collections/Concurrent/ConcurrentDistinctInOrderCollection/ConcurrentDistinctInOrderCollection.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using GreenSuperGreen.UnifiedConcurrency;
+
+namespace GreenSuperGreen.Collections.Concurrent
+{
+	public class ConcurrentDistinctInOrderCollection<T> : IConcurrentDistinctInOrderCollection<T>
+	{
+		private ILockUC Lock { get; } = new SpinLockUC();
+		private ISet<T> DistinctSet { get; } = new HashSet<T>();
+		private ImmutableArray<T> DistinctInOrderArrayStorage { get; set; } = ImmutableArray<T>.Empty;
+
+		public ConcurrentDistinctInOrderCollection()
+		{
+		}
+
+		public ConcurrentDistinctInOrderCollection(IEnumerable<T> items)
+		{
+			items
+			.Where(DistinctSet.Add)
+			.ToImmutableArray()
+			.Assign(arr => DistinctInOrderArrayStorage = arr)
+			;
+		}
+
+		public ImmutableArray<T> DistinctInOrderArray
+		{
+			get
+			{
+				using (Lock.Enter()) return DistinctInOrderArrayStorage;
+			}
+		}
+
+		public ImmutableArray<T> Add(T item)
+		{
+			using (Lock.Enter())
+			{
+				return DistinctInOrderArrayStorage = DistinctSet.Add(item)
+				? DistinctInOrderArrayStorage.Add(item)
+				: DistinctInOrderArrayStorage
+				;
+			}
+		}
+
+		public ImmutableArray<T> AddRange(IEnumerable<T> items)
+		{
+			ImmutableArray<T> arr = (items ?? Enumerable.Empty<T>()).ToImmutableArray();
+
+			using (Lock.Enter())
+			{
+				if (arr.IsEmpty) return DistinctInOrderArrayStorage;
+
+				arr
+				.Where(DistinctSet.Add)
+				.ToImmutableArray()
+				.AssignOut(out arr)
+				;
+
+				return arr.IsEmpty
+				? DistinctInOrderArrayStorage
+				: DistinctInOrderArrayStorage = DistinctInOrderArrayStorage.AddRange(arr)
+				;
+			}
+		}
+
+		public ImmutableArray<T> Remove(T item)
+		{
+			using (Lock.Enter())
+			{
+				return DistinctSet.Remove(item)
+				? DistinctInOrderArrayStorage = DistinctInOrderArrayStorage.Remove(item)
+				: DistinctInOrderArrayStorage
+				;
+			}
+		}
+
+		public bool IsEmpty
+		{
+			get
+			{
+				using (Lock.Enter())
+				{
+					return DistinctInOrderArrayStorage.IsEmpty;
+				}
+			}
+		}
+
+		public ImmutableArray<T> Clear()
+		{
+			using (Lock.Enter())
+			{
+				DistinctSet.Clear();
+				return DistinctInOrderArrayStorage = ImmutableArray<T>.Empty;
+			}
+		}
+	}
+}
---
+++ GreenSuperGreen.Benchmarking.NetStandard/Collections/Concurrent/ConcurrentDistinctInOrderCollection/IConcurrentDistinctInOrderCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace GreenSuperGreen.Collections.Concurrent
+{
+	public interface IConcurrentDistinctInOrderCollection<T>
+	{
+		ImmutableArray<T> DistinctInOrderArray { get; }
+		bool IsEmpty { get; }
+		ImmutableArray<T> Add(T item);
+		ImmutableArray<T> AddRange(IEnumerable<T> items);
+		ImmutableArray<T> Remove(T item);
+		ImmutableArray<T> Clear();
+	}
+}
