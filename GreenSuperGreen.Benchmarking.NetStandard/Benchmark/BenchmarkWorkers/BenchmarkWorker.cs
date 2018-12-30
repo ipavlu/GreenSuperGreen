1dad5d37d76d7946ef5f74286a602c5ba3f32782
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-12-30 05:37:21 +0100

    BenchmarkWorkers/IBenchmarkWorkers:
    Basic Benchmark Worker, knows how to perform execution and timing, but target is not implemented. BenchmarkWorkers class inherits from BenchmarkConfiguration in order to easily get access to specific configuration for execution.

---
+++ GreenSuperGreen.Benchmarking.NetStandard/Benchmark/BenchmarkWorkers/BenchmarkWorker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace GreenSuperGreen.Benchmarking
+{
+	public abstract class BenchmarkWorker : BenchmarkConfiguration, IBenchmarkWorker
+	{
+		private Stopwatch StopWatchTimer { get; set; } = new Stopwatch();
+
+		public long ElapsedMilliseconds { get; private set; }
+		public int Iterations { get; private set; }
+		public string Pair { get; }
+		public double ThroughputPerMillisecond => ElapsedMilliseconds / (double)(Iterations <= 0 ? 1 : Iterations);
+
+		protected BenchmarkWorker(IBenchmarkConfiguration benchmarkConfiguration, string pair = null)
+		:	base(benchmarkConfiguration)
+		{
+			Pair = pair ?? string.Empty;
+		}
+
+		protected void WastingTime()
+		{
+			// ReSharper disable once EmptyForStatement
+			// ReSharper disable once SuggestVarOrType_BuiltInTypes
+			for (long t = 0; t < Spins; ++t) { }
+		}
+
+		protected virtual async Task BenchmarkingTarget() { await Task.CompletedTask; throw new NotImplementedException(); }
+
+		public Task RunProcessing() => Task.Run(Processing);
+
+		protected async Task Processing()
+		{
+			StopWatchTimer.Start();
+			TimeSpan? next = null;
+			TimeSpan now;
+			do
+			{
+				await BenchmarkingTarget();
+				Iterations++;
+
+				now = StopWatchTimer.Elapsed;
+				next = now > next ? now + PerfCollector.TryRead(): next ?? (now + PerfCollector.TryRead());
+			} while (now < TimeSpan);
+
+			StopWatchTimer.Stop();
+			ElapsedMilliseconds = StopWatchTimer.ElapsedMilliseconds;
+			StopWatchTimer = null;
+		}
+	}
+}
---
+++ GreenSuperGreen.Benchmarking.NetStandard/Benchmark/BenchmarkWorkers/IBenchmarkWorker.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace GreenSuperGreen.Benchmarking
+{
+	public interface IBenchmarkWorker
+	{
+		long ElapsedMilliseconds { get; }
+		int Iterations { get; }
+		string Pair { get; }
+		double ThroughputPerMillisecond { get; }
+
+		Task RunProcessing();
+	}
+}
