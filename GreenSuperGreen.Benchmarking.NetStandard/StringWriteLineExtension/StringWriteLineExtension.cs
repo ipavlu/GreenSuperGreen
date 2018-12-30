4004e9dd277e254619dcc6976752e4d686e84140
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-12-30 05:55:44 +0100

    Simple Console.WriteLine extension to ensure smooth tex

---
+++ GreenSuperGreen.Benchmarking.NetStandard/StringWriteLineExtension/StringWriteLineExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace GreenSuperGreen.Benchmarking
+{
+	public static class StringWriteLineExtension
+	{
+		/// <summary>  Write string to <see cref="System.Console.WriteLine"/>  </summary>
+		public static void WriteLine(this string msg) => System.Console.WriteLine(msg);
+	}
+}
