e480d133a09c5dcdee0797d520db49cd7a7c28ec
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-12-30 05:25:04 +0100

    New start up point for Benchmarks: BenchmarkingInitPoint

---
+++ GreenSuperGreen.Benchmarking.NetStandard/BenchmarkingInitPoint.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using GreenSuperGreen.Diagnostics;
+using GreenSuperGreen.TextWriterReplication;
+
+// ReSharper disable ForCanBeConvertedToForeach
+// ReSharper disable LoopCanBeConvertedToQuery
+// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace GreenSuperGreen.Benchmarking
+{
+	public static class BenchmarkingInitPoint
+	{
+		private static string TestsMenu { get; } =
+		BenchmarkManagersConfig
+		.BenchmarkManagers
+		.Select(benchmarkManager => $"{benchmarkManager.KeyName}: {benchmarkManager.Name}{Environment.NewLine}")
+		.Aggregate(string.Empty, (c, n) => c + n)
+		;
+
+		private static void PrintTestMenu() => TestsMenu.WriteLine();
+
+		public static void PrintIntro() => "Enjoy! GreenSuperGreen.Benchmarking.NetStandard ipavlu 2017".WriteLine();
+		public static void PrintExit() => "Exit: GreenSuperGreen.Benchmarking.NetStandard ipavlu 2017".WriteLine();
+
+		public static async Task MainAsync(string[] args)
+		{
+			using (ITextWriter textWriter = new TextWriterReplicatorManager().InstallConsole(DisposeAncestor.Yes))
+			{
+				await BenchProcessing(args, textWriter);
+			}
+		}
+
+		private static async Task BenchProcessing(string[] args, ITextWriter textWriter)
+		{
+			IPerfCounterCollectorUC perfCollector =
+			PerfCounterTypeUC.ProcessorTime.PerfCounterCollector(TimeSpan.FromMilliseconds(1000), Process.GetCurrentProcess().ProcessName)
+			;
+
+			if (args.Any(str => str.Contains("collect")))
+			{
+				await DataCollectorBenchmark.CollectData(perfCollector, textWriter);
+				return;
+			}
+
+			Process.GetCurrentProcess().ProcessName.WriteLine();
+			PrintIntro();
+
+			string.Empty.WriteLine();
+			string.Empty.WriteLine();
+
+			await ManualSelector(perfCollector, textWriter);
+
+			PrintExit();
+		}
+
+		private static async Task ManualSelector(IPerfCounterCollectorUC perfCollector, ITextWriter textWriter)
+		{
+			while (true)
+			{
+				PrintTestMenu();
+
+				$"Select tests or [Exit]/[Quit]:{Environment.NewLine}".WriteLine();
+
+				(System.Console.ReadLine() ?? string.Empty)
+				.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+				.Select(str => str.ToLower())
+				.Where(str => !string.IsNullOrEmpty(str) && !string.IsNullOrWhiteSpace(str))
+				.Distinct()
+				.ToImmutableHashSet()
+				.AssignOut(out ImmutableHashSet<string> benchmarkSelector)
+				.Select(BenchmarkManagersConfig.GetBenchmarkManager)
+				.Where(benchmarkManager => benchmarkSelector != null)
+				.ToImmutableArray()
+				.AssignOut(out ImmutableArray<IBenchmarkManager> selectedBenchmarkManagers)
+				;
+
+				if (benchmarkSelector.Contains("exit") || benchmarkSelector.Contains("quit"))
+				{
+					"Requested Exit".WriteLine();
+					return;
+				}
+
+				int spins = 0;
+
+
+				if (	selectedBenchmarkManagers
+					    .Where(bManager => bManager.IsDataCollector)
+					    .ToImmutableArray()
+					    .AssignOut(out ImmutableArray<IBenchmarkManager> dataCollector)
+					    .Length > 0)
+				{
+					selectedBenchmarkManagers = dataCollector;
+				}
+				else
+				{
+					$"Select spin time:{Environment.NewLine}".WriteLine();
+					string spinText = System.Console.ReadLine() ?? string.Empty;
+					if (!int.TryParse(spinText, out spins) || spins < 0) continue;
+				}
+
+				foreach (IBenchmarkManager test in selectedBenchmarkManagers)
+				{
+					string.Empty.WriteLine();
+
+					await test.ExecuteBenchmark(new BenchmarkConfiguration(test, perfCollector, BenchmarkGlobalSettings.TestingTimeSpan, spins, textWriter));
+
+					string.Empty.WriteLine();
+					await Task.Delay(2000);
+				}
+			}
+		}
+	}
+}
