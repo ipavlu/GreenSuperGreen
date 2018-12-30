36087b83f167dc7f3e726ab44524bc5db0ce3d8f
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-12-30 05:31:53 +0100

    ThreadGroupIndex: Representing a thread group, in group there can be only one thread or more.
    Heavy scenario has one thread per group and all groups share same synchronization primitive.
    
    BadNeighbor scenarios crate groups, each has pair of distinct threads and each pair/group has own synchronization primitive.

---
+++ GreenSuperGreen.Benchmarking.NetStandard/Benchmark/ThreadGroupIndex/ThreadGroupIndex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreenSuperGreen.Benchmarking
+{
+	public interface IThreadGroupIndex
+	{
+		int Id { get; }
+	}
+
+	public class ThreadGroupIndex : IThreadGroupIndex, IEquatable<ThreadGroupIndex>
+	{
+		public static IThreadGroupIndex New(int id) => new ThreadGroupIndex(id);
+
+		public int Id { get; }
+
+		public ThreadGroupIndex(int threadGroupIndex) => Id = threadGroupIndex;
+
+		public override bool Equals(object obj) => Equals(obj as ThreadGroupIndex);
+
+		public bool Equals(ThreadGroupIndex other) => other != null && Id == other.Id;
+
+		public override int GetHashCode() => Id;
+
+		public override string ToString() => $"{nameof(ThreadGroupIndex)}[{Id}]";
+
+		public static bool operator ==(ThreadGroupIndex index1, ThreadGroupIndex index2)
+		=> EqualityComparer<ThreadGroupIndex>.Default.Equals(index1, index2)
+		;
+
+		public static
+		bool operator !=(ThreadGroupIndex index1, ThreadGroupIndex index2) => !(index1 == index2);
+	}
+}
