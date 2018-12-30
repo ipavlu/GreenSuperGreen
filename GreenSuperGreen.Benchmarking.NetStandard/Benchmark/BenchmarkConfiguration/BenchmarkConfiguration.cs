72bec6ba5e4e28d5c2f7ac9e17907c5fa22a8419
ipavlu <29397497+ipavlu@users.noreply.github.com>
2018-12-30 05:45:19 +0100

    BenchmarkConfiguration : IBenchmarkConfiguration responsible to permeate information about specific Benchmark run, spins, time, etc

---
+++ GreenSuperGreen.Benchmarking.NetStandard/Benchmark/BenchmarkConfiguration/BenchmarkConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using GreenSuperGreen.Diagnostics;
+using GreenSuperGreen.TextWriterReplication;
+
+namespace GreenSuperGreen.Benchmarking
+{
+	public class BenchmarkConfiguration : IBenchmarkConfiguration
+	{
+		public IBenchmarkManager BenchmarkManager { get; }
+		public IPerfCounterCollectorUC PerfCollector { get; }
+		public TimeSpan TimeSpan { get; }
+		public long Spins { get; }
+		public ITextWriter TextWriter { get; }
+
+
+		public BenchmarkConfiguration(
+			IBenchmarkManager benchmarkManager,
+			IPerfCounterCollectorUC perfCollector,
+			TimeSpan timeSpan,
+			long spins,
+			ITextWriter textWriter = null)
+		{
+			BenchmarkManager = benchmarkManager ?? throw new ArgumentNullException(nameof(benchmarkManager));
+			PerfCollector = perfCollector ?? throw new ArgumentNullException(nameof(perfCollector));
+			TimeSpan = timeSpan;
+			Spins = spins;
+			TextWriter = textWriter;
+		}
+
+		public BenchmarkConfiguration(IBenchmarkConfiguration benchmarkConfiguration)
+		{
+			BenchmarkManager = benchmarkConfiguration?.BenchmarkManager ?? throw new ArgumentNullException(nameof(benchmarkConfiguration.BenchmarkManager));
+			PerfCollector = benchmarkConfiguration?.PerfCollector ?? throw new ArgumentNullException(nameof(benchmarkConfiguration.PerfCollector));
+			TimeSpan = benchmarkConfiguration?.TimeSpan ?? BenchmarkGlobalSettings.TestingTimeSpan;
+			Spins = benchmarkConfiguration?.Spins ?? BenchmarkGlobalSettings.TestingSpins;
+			TextWriter = benchmarkConfiguration?.TextWriter;
+		}
+	}
+}
---
+++ GreenSuperGreen.Benchmarking.NetStandard/Benchmark/BenchmarkConfiguration/IBenchmarkConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GreenSuperGreen.Diagnostics;
+using GreenSuperGreen.TextWriterReplication;
+
+namespace GreenSuperGreen.Benchmarking
+{
+	public interface IBenchmarkConfiguration
+	{
+		IBenchmarkManager BenchmarkManager { get; }
+		IPerfCounterCollectorUC PerfCollector { get; }
+		TimeSpan TimeSpan { get; }
+		long Spins { get; }
+		ITextWriter TextWriter { get; }
+	}
+}
